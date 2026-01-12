aab2ee327809f3004987f9fec715d17b04816d7f
Saurabh Kumar <115585845+ashivsaurabh@users.noreply.github.com>
2026-01-12 15:45:55 +0000

    math

---
+++ Notes/alljs/Numbers.js
@@ -0,0 +1,8 @@
+const a=12
+const b=new Number(100)
+console.log(a);
+console.log(b.toString().length)
+console.log(b.toFixed(5));
+console.log(b.toPrecision(1))
+const e=100000
+console.log(e.toLocaleString())
---
+++ Notes/string.js
@@ -0,0 +1,6 @@
+const a="hello"
+const b="how are you"
+console.log(`say ${a} and ${b}`);
+let a1=20,b1n=30;
+console.log(a1 + b1n);
+console.log(`the value of ${a1}and ${b1n}is ${a1+b1n}`)
