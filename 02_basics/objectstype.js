3e5f1f17e52236d5a78826f680261a176c8973d7
Saurabh Kumar <115585845+ashivsaurabh@users.noreply.github.com>
2026-01-14 15:17:56 +0000

    functions

---
+++ 02_basics/objectstype.js
@@ -0,0 +1,39 @@
+const party={
+    name:"johny",
+    age:12,
+    sex:"m"
+}
+const {age}=party
+console.log(age);
+
+const data={
+    name:"cena",
+    work:"fight",
+    company:"wwe"
+}
+
+const {company:c}=data
+console.log(c);
+
+const array=["1",22,"HELLO"]
+console.log(array);
+const array2=[1,2,3,4,5,6,7,8]
+const array5=[...array,...array2]
+console.log(array5);
+
+
+json 
+
+{
+    name:"hello",
+    age:12
+}
+
+or 
+
+{
+    "name":"hello",
+    "age":12
+}
+
+
